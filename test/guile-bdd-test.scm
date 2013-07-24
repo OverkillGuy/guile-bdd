28ab03bb9540cc697d121aee24c5bf27e472bf14
Cyrill Schenkel <cyrill.schenkel@gmail.com>
2013-07-24 23:14:15 +0200

    added test directory structure

--- test/guile-bdd-test.scm
+++ test/guile-bdd-test.scm
@@ -16,4 +16,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see {http://www.gnu.org/licenses/}.
 
-(define-module (guile-bdd))
+(use-modules (ice-9 format)
+	     (guile-bdd))
+
+
--- test/run-all-tests.sh
+++ test/run-all-tests.sh
@@ -1,21 +1,23 @@
-#!/bin/bash
+#!/usr/bin/guile \
+-L ../src/
+!#
 
-# guile-bdd - A behavior driven development framework for GNU Guile
-# Copyright (C) 2013  Cyrill Schenkel
-#
-# This file is part of guile-bdd.
-#
-# guile-bdd is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# guile-bdd is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see {http://www.gnu.org/licenses/}.
+;; guile-bdd - A behavior driven development framework for GNU Guile
+;; Copyright (C) 2013  Cyrill Schenkel
+;;
+;; This file is part of guile-bdd.
+;;
+;; guile-bdd is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; guile-bdd is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see {http://www.gnu.org/licenses/}.
 
-return 0
+(load "guile-bdd-test.scm")
